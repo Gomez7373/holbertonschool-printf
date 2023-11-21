d0d2307aa411d7ff42aea0943958f7ea97fbd7ab
Gomez7373 <sgc.holberton@gmail.com>
2023-11-21 15:14:26 -0400

    first atempt to 1

---
+++ d_printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * d_printf - Print a decimal (same as integer)
+ * @args: Argument list
+ *
+ * Return: Number of characters printed
+ */
+int d_printf(va_list args)
+{
+    return i_printf(args);
+}
+
