760f6f2ac1c9bfde4990720ddb0451884e005b11
Gomez7373 <sgc.holberton@gmail.com>
2023-11-22 19:09:13 -0400

    _printf2

--- main.c
+++ main.c
@@ -14,31 +14,62 @@ int main(void)
     unsigned int ui;
     void *addr;
 
+    /* Test Case 1: Basic string printing*/
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
+
+    /* Test Case 2: Printing lengths using %d and %i*/
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
+
+    /* Test Case 3: Printing negative integers*/
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
+
+    /* Test Case 4: Printing unsigned integers*/
+    ui = (unsigned int)INT_MAX + 1024;
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
+
+    /* Test Case 5: Printing unsigned octal*/
     _printf("Unsigned octal:[%o]\n", ui);
     printf("Unsigned octal:[%o]\n", ui);
+
+    /* Test Case 6: Printing unsigned hexadecimal*/
     _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+
+    /* Test Case 7: Printing characters*/
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
+
+    /* Test Case 8: Printing strings*/
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
+
+    /* Test Case 9: Printing addresses*/
+    addr = (void *)0x7ffe637541f0;
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
+
+    /* Test Case 10: Printing percent sign*/
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+
+    /* Test Case 11: Testing unknown specifier (%r assumed)*/
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+
+/* Test Case 12: Testing with INT_MAX*/
+    _printf("Max Integer:[%d]\n", INT_MAX);
+    printf("Max Integer:[%d]\n", INT_MAX);
+
+    /* Test Case 13: Testing with INT_MIN*/
+    _printf("Min Integer:[%d]\n", INT_MIN);
+    printf("Min Integer:[%d]\n", INT_MIN);
+
     return (0);
 }
+
---
+++ print_unsigned_integer.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_unsigned_integer - Print an unsigned integer
+ * @num: The unsigned integer to be printed
+ *
+ * Return: Number of characters printed
+ */
+int print_unsigned_integer(unsigned int num)
+{
+    int count = 0;
+
+    if (num / 10)
+        count += print_unsigned_integer(num / 10);
+
+    _putchar(num % 10 + '0');
+    return count + 1;
+}
+
