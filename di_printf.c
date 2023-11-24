002ca5609f823dc14015872addef832572462240
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-24 06:47:25 -0400

    test di_printf

--- di_printf.c
+++ di_printf.c
@@ -10,9 +10,37 @@
 int di_printf(va_list args)
 
 {
+	int num = va_arg(args, int);
 
-int num = va_arg(args, int);
+	int count = 0;
+	unsigned int abs_min;
 
-return (print_integer(num));
+	if (num == INT_MIN)
+	{
+		_putchar('-');
+		count++;
+		abs_min = INT_MIN;
+		count += print_integer(abs_min / 10);
+		_putchar(abs_min % 10 + '0');
+		count++;
+	}
 
+	else
+	{
+
+		if (num < 0)
+		{
+			_putchar('-');
+			count++;
+			num = -num;
+		}
+
+		if (num / 10)
+			count += print_integer(num / 10);
+
+		_putchar(num % 10 + '0');
+		count++;
+	}
+
+	return (count);
 }
