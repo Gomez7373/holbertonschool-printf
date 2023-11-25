10083ca96eeae4653ac3d6ab35312d0b246ec674
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-25 13:41:59 -0400

    updating files

--- di_printf.c
+++ di_printf.c
@@ -7,16 +7,9 @@
  * Return: Number of characters printed
  */
 
-int di_printf(va_list args, int uppercase, int flag_plus, int flag_space, int flag_hash)
-
+int di_printf(va_list args)
 {
-	int num;
-	(void)uppercase;
-	(void)flag_plus;
-	(void)flag_space;
-	(void)flag_hash;
-
-	num = va_arg(args, int);
+	int num = va_arg(args, int);
 
 	return (print_integer(num));
 
