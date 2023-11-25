8e53b6c4bd56e0d2a96af7df378e4e7bf28595ea
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-25 12:28:51 -0400

    Trying task 8

--- b_printf.c
+++ b_printf.c
@@ -6,29 +6,34 @@
  *
  * Return: Number of characters printed
  */
-int b_printf(va_list args)
+int b_printf(va_list args, int uppercase, int flag_plus, int flag_space, int flag_hash)
 {
-unsigned int num = va_arg(args, unsigned int);
-int binary_digits[32];
-int i, count = 0;
+	unsigned int num;
+	int binary_digits[32];
+	int i, count = 0;
+	(void)uppercase;
+	(void)flag_plus;
+	(void)flag_space;
+	(void)flag_hash;
 
-if (num == 0)
-{
-_putchar('0');
-return (1);
-}
+	num = va_arg(args, unsigned int);
+	
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-for (i = 0; num > 0; i++)
-{
-binary_digits[i] = num % 2;
-num /= 2;
-}
-
-for (i -= 1; i >= 0; i--)
-{
-_putchar(binary_digits[i] + '0');
-count++;
-}
-
-return (count);
+	for (i = 0; num > 0; i++)
+	{
+		binary_digits[i] = num % 2;
+		num /= 2;
+	}
+	
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(binary_digits[i] + '0');
+		count++;
+	}
+	return (count);
 }
