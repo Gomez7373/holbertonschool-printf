7d55c28c001f7798d50b74e498d776b88b1bf4ab
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-25 12:05:55 -0400

    Trying task 8

--- s_printf.c
+++ s_printf.c
@@ -6,37 +6,40 @@
 *
 * Return: Number of characters in the string
 */
-int s_printf(va_list args)
+int s_printf(va_list args, int uppercase, int flag_plus, int flag_space, int flag_hash)
 {
-char *s;
-int x = 0;
+	char *s;
+	int x = 0;
+	(void)uppercase;
+	(void)flag_plus;
+	(void)flag_space;
+	(void)flag_hash;
 
 /* Extract the string argument from the variable arguments list */
-s = va_arg(args, char *);
+	s = va_arg(args, char *);
 
 /* Check if the string is NULL */
-if (!s)
-{
-s = "(null)";
+	if (!s)
+	{
+		s = "(null)";
 
 /* Print each character of the "(null)" string */
-for (x = 0; s[x] != '\0'; x++)
-{
-_putchar(s[x]);
-}
-
-/* Return the length of "(null)" */
-return (6);  /* Assuming you are returning the length of "(null)" */
-}
-else
-{
+		for (x = 0; s[x] != '\0'; x++)
+		{
+			_putchar(s[x]);
+		}
+		/* Return the length of "(null)" */
+		return (6);  /* Assuming you are returning the length of "(null)" */
+	}
+	else
+	{
 /* Print each character of the provided string */
-for (x = 0; s[x] != '\0'; x++)
-{
-_putchar(s[x]);
-}
+		for (x = 0; s[x] != '\0'; x++)
+		{
+			_putchar(s[x]);
+		}
 
 /* Return the actual length of the string */
-return (x);
-}
+		return (x);
+	}
 }
